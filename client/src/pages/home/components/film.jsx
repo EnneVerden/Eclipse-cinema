941dc907cce978877f51c71a791402ca66811677
Pavel <42992218+EnneVerden@users.noreply.github.com>
2019-07-14 01:07:26 +0300

    Updated website design.

--- client/src/pages/home/components/film.jsx
+++ client/src/pages/home/components/film.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 
 const Film = () => (
   <div className="films-grid__film">
-    <img
-      src="https://m.media-amazon.com/images/M/MV5BNzY1MDA2OTQ0N15BMl5BanBnXkFtZTgwMTkzNjU2NTM@._V1_SY1000_CR0,0,631,1000_AL_.jpg"
-      alt="Film poster"
-      className="films-grid__img"
-    />
-    <span className="films-grid__name">Mortal Engines</span>
-    <div className="films-grid__info">
-      <span className="films-grid__price">5$</span>
-      <button type="button" className="btn films-grid__btn">
-        <i className="fas fa-shopping-cart" />
-      </button>
+    <div className="films-grid__img-block">
+      <img
+        src="https://m.media-amazon.com/images/M/MV5BNzY1MDA2OTQ0N15BMl5BanBnXkFtZTgwMTkzNjU2NTM@._V1_SY1000_CR0,0,631,1000_AL_.jpg"
+        alt="Film poster"
+        className="films-grid__img"
+      />
+    </div>
+    <div className="films-grid__info-block">
+      <span className="films-grid__title">Mortal Engines</span>
+      <span className="films-grid__categories">Action, Adventure, Fantasy</span>
+      <div className="films-grid__divide-block">
+        <span className="films-grid__description">
+          In a post-apocalyptic world where cities ride on wheels and consume
+          each other to survive, two people meet in London and try to stop a
+          conspiracy.
+        </span>
+        <div className="films-grid__action-block">
+          <span className="films-grid__price">5$</span>
+          <span className="films-grid__date">15.07.2019 - 21.07.2019</span>
+          <button type="button" className="btn films-grid__btn">
+            <i className="fas fa-shopping-cart" />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 );
--- client/src/shared/header/components/logo.jsx
+++ client/src/shared/header/components/logo.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Logo = () => (
   <div className="header__logo">
     <a href="/home" className="header__logo-link">
-      <img src="./logo.png" alt="Logo" className="header__logo-img" />
       <span>Eclipse</span>
     </a>
   </div>
