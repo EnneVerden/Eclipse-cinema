dd655b863001788772753ded5f2dfe5b0e09229c
Pavel <42992218+EnneVerden@users.noreply.github.com>
2019-07-19 23:37:07 +0300

    Added preloader when loading movies.

--- client/src/pages/home/components/filmsGrid.jsx
+++ client/src/pages/home/components/filmsGrid.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Film from './film';
+import Preloader from '../../../shared/preloader/Preloader';
 
 const FilmsGrid = ({ filmsData }) => {
   const film = filmsData.map(item => <Film key={item._id} film={item} />);
 
+  const content = filmsData.length ? film : <Preloader />;
+
   return (
     <section className="films-grid">
       <div className="films-grid__grid">
-        <div className="films-grid__row">{film}</div>
+        <div className="films-grid__row">{content}</div>
       </div>
     </section>
   );
---
+++ client/src/shared/preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => (
+  <div className="lds-css ng-scope">
+    <div className="lds-eclipse">
+      <div />
+    </div>
+  </div>
+);
+
+export default Preloader;
